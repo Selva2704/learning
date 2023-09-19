c4110e4b18edc6b6555af6ceea9adc1ea9b41d41
Selva2704 <145125797+Selva2704@users.noreply.github.com>
2023-09-19 21:44:59 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
 echo "sample text for task"
-echo "adding other line"
-echo "adding 2nd line"
-echo "adding 3rd line"
-echo "adding 4th line"
-echo "adding 5th line"
-echo "adding 6th line"
-echo "abcd"
-
-echo "adding 7th line"
-echo "success"
