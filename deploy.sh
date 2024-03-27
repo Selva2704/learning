d1a24e4ce0320135aaa9542c0368a94277b76ed5
Selva2704 <145125797+Selva2704@users.noreply.github.com>
2024-03-28 01:34:29 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 docker-compose up -d
+CONTAINER_NAME='democontainer'
+x=$(docker ps -q -f status=running -f name=$CONTAINER_NAME)
+if [[ -n "$x" ]]; then
+  echo 'Container exists'
+else
+  echo 'No container image'
+fi
