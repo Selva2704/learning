ed92a1cac4d128a6319dfc49036e194bd786eac0
Selva2704 <145125797+Selva2704@users.noreply.github.com>
2024-03-28 00:31:48 +0530

    Update pushtohub.sh

--- pushtohub.sh
+++ pushtohub.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-docker tag project:v1 selvadev27/dev
-docker tag nginx-demo:latest selvadev27/dev
-docker push selvadev27/dev
-docker tag nginx-demo:latest selvadev27/prod
-docker push selvadev27/prod
+docker tag mynginx:v01 selvadev27:new
+docker push selvadev27:new
+
